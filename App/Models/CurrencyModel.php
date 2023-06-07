a0e774d2914cb8b8554bfba19eb4527d399075f2
szymon cieszynski <cieszynski8@gmail.com>
2023-06-07 12:52:30 +0200

    Change API and displaying in conversion page

--- App/Models/CurrencyModel.php
+++ App/Models/CurrencyModel.php
@@ -14,46 +14,74 @@ class CurrencyModel extends \Core\Model
 
     public function fetchDataFromNBP()
     {
-        $apiUrl = 'http://api.nbp.pl/api/exchangerates/tables/A/';
+        //$apiUrl = 'http://api.nbp.pl/api/exchangerates/tables/A/';
+        $apiUrl = 'https://api.nbp.pl/api/exchangerates/tables/c/';
             
         // Fetching data from API
         $jsonResponse = file_get_contents($apiUrl);
         //var_dump($jsonResponse);
         $data = json_decode($jsonResponse, true);
 
-        $filteredData = array_map(function($rate) {
+        $localCurrency = array(
+            'currency' => 'Polski złoty',
+            'code' => 'PLN',
+            'bid' => '1.00',
+            'ask' => '1.00'
+        );
+
+        $foreignCurrencies = array_map(function($rate) {
             return array(
                 'currency' => $rate['currency'],
                 'code' => $rate['code'],
-                'mid' => $rate['mid']
+                'bid' => $rate['bid'],
+                'ask' => $rate['ask'],
             );
         }, $data[0]['rates']);
     
-        return $filteredData;
+    
+        $combinedData = array(
+            'localCurrency' => $localCurrency,
+            'foreignCurrencies' => $foreignCurrencies
+            
+        );
+        
+       // var_dump($combinedData);
+        return $combinedData;
     }
 
     public function saveDataToDatabase($data)
     {
 
-        $sql = 'INSERT INTO currency (currency, code, mid)
-        VALUES (:currency, :code, :mid) ON DUPLICATE KEY UPDATE mid = :mid';
+        $sql = 'INSERT INTO currency (currency, code, bid, ask)
+        VALUES (:currency, :code, :bid, :ask) ON DUPLICATE KEY UPDATE currency = :currency';
 
         $db = static::getDB();
         $stmt = $db->prepare($sql);
 
-        foreach ($data as $currencyData) {
-            if (isset($currencyData['currency']) && isset($currencyData['code']) && isset($currencyData['mid'])) {
+        if (isset($data['localCurrency']) && isset($data['localCurrency']['currency']) && isset($data['localCurrency']['code']) && isset($data['localCurrency']['bid']) && isset($data['localCurrency']['ask'])) {
+            $localCurrencyData = $data['localCurrency'];
+            $stmt->bindValue(':currency', $localCurrencyData['currency'], PDO::PARAM_STR);
+            $stmt->bindValue(':code', $localCurrencyData['code'], PDO::PARAM_STR);
+            $stmt->bindValue(':bid', $localCurrencyData['bid'], PDO::PARAM_STR);
+            $stmt->bindValue(':ask', $localCurrencyData['ask'], PDO::PARAM_STR);
+            $stmt->execute();
+        }
+
+        foreach ($data['foreignCurrencies'] as $currencyData) {
+            if (isset($currencyData['currency']) && isset($currencyData['code']) && isset($currencyData['bid']) && isset($currencyData['ask'])) {
                 $stmt->bindValue(':currency', $currencyData['currency'], PDO::PARAM_STR);
                 $stmt->bindValue(':code', $currencyData['code'], PDO::PARAM_STR);
-                $stmt->bindValue(':mid', $currencyData['mid'], PDO::PARAM_STR);
+                $stmt->bindValue(':bid', $currencyData['bid'], PDO::PARAM_STR);
+                $stmt->bindValue(':ask', $currencyData['ask'], PDO::PARAM_STR);
                 $stmt->execute();
             }
         }
+    
     }
 
     public function getDataFromDatabase()
     {
-        $sql = 'SELECT currency, code, mid FROM currency';
+        $sql = 'SELECT * FROM currency ORDER BY CASE WHEN code = "PLN" THEN 0 ELSE 1 END, code ASC';
         $db = static::getDB();
         $stmt = $db->query($sql);
         
